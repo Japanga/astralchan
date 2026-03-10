bd9849ef14b25bf1c86ee765e687ee8e9ceb6f4b
Japanga <japanganemo@gmail.com>
2026-03-10 13:34:43 -0500

    Update thread.php

--- boards/b/thread.php
+++ boards/b/thread.php
@@ -278,18 +278,7 @@ function setReplyId(id) {
   
 <script src="greentextandyoutube.js"></script>
     
-    <script>
-document.addEventListener("DOMContentLoaded", function() {
-    // Regex: Match # followed by one or more digits (\d+)
-    const regex = /#(\d+)/g;
-    
-    // Replace function: wraps the matched text in an <a> tag
-    // $1 refers to the numbers, e.g., in #123, it's 123
-    document.body.innerHTML = document.body.innerHTML.replace(regex, 
-        '<b><a href="#$1" class="hash-link">#$1</a></b>');
-});
-
-    </script>
+<script src="tagging.js"></script>
 
     
        <style>
